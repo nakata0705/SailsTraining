fd9a04244e9d72e91f4f8cbff935d3421b9e0a85
nakata0705 <nakata0705@gmail.com>
2015-03-29 10:35:49 -0700

    Use findOne instead of find. Etc...

--- api/models/File.js
+++ api/models/File.js
@@ -1,20 +1,48 @@
 /**
-* File.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * File.js
+ *
+ * @description :: This model manages files in this program. Controller manages the database entry and the real file will be managed by the Lifecycle callbacks.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
 
 module.exports = {
+    attributes: {
+        hash: {type: "string", unique: true, required: true},
+        name: {type: "string", columnName: "name", defaultsTo: "New File"},
+        type: {type: "string", enum: ["file", "directory"], defaultsTo: "file"},
+        path: {type: "string", required: true},
+        owner: {model: "user", required: true},
+        parent: {model: "file"},
+        project: {model: "project", required: true}
+    },
 
-  attributes: {
-      hash: { type: "string", unique: true },
-      name: { type: "string", columnName: "name", defaultsTo: "New File" },
-      type: { type: "string", enum: ["file", "directory"] },
-      path: { type: "string" },
-      owner: { model: "user" },
-      parent: { model: "file" },
-      project: { model: "project" }
-  }
-};
-
+    afterCreate: function (values, callback) {
+        switch (values.type) {
+            case "directory":
+                mkdirp(values.path, function (err) {
+                    if (err) {
+                        console.error(err);
+                        File.destroy({hash: values.hash}).exec(function (err_destroy) {
+                            callback(err);
+                        });
+                    }
+                    callback();
+                });
+                break;
+            default:
+                fs.open(values.path, 'w', function (err) {
+                    if (err) {
+                        console.error(err);
+                        File.destroy({hash: values.hash}).exec(function (err_destroy) {
+                            callback(err);
+                        });
+                    }
+                    callback();
+                });
+        }
+    }
+}
